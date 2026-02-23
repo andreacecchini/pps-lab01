f99598d6c9c3fd1c5fbc642c5c20ce827ca516f6
Andrea Cecchini <cecchiniandrea@outlook.it>
2026-02-23 19:07:46 +0100

    feat: it is not possible to deposit a negative amount

--- basic-refactoring-exercise/src/test/java/it/unibo/pps/model/SimpleBankAccountTest.java
+++ basic-refactoring-exercise/src/test/java/it/unibo/pps/model/SimpleBankAccountTest.java
@@ -12,6 +12,7 @@ class SimpleBankAccountTest {
     private static final int INITIAL_BALANCE = 0;
     private static final int DEPOSIT_AMOUNT = 100;
     private static final int WITHDRAWAL_AMOUNT = 70;
+    private static final int NEGATIVE_AMOUNT = -1;
     private static final String ACCOUNT_HOLDER_NAME = "Mario";
     private static final String ACCOUNT_HOLDER_SURNAME = "Rossi";
     private static final int ACCOUNT_HOLDER_ID = 1;
@@ -42,6 +43,12 @@ class SimpleBankAccountTest {
         bankAccount.deposit(accountHolder.id(), DEPOSIT_AMOUNT);
         bankAccount.deposit(WRONG_ACCOUNT_HOLDER_ID, wrongDepositAmount);
         assertEquals(DEPOSIT_AMOUNT, bankAccount.getBalance());
+    }
+
+    @Test
+    void testShouldNotBePossibleToDepositANegativeAmount() {
+        bankAccount.deposit(accountHolder.id(), NEGATIVE_AMOUNT);
+        assertEquals(INITIAL_BALANCE, bankAccount.getBalance());
     }
 
     @Test
