d65ad0deb988125b11161b44d511fba6141829a0
Andrea Cecchini <cecchiniandrea@outlook.it>
2026-02-23 19:55:34 +0100

    feat: added fee on withdrawals

--- basic-refactoring-exercise/src/main/java/it/unibo/pps/model/FeeBankAccount.java
+++ basic-refactoring-exercise/src/main/java/it/unibo/pps/model/FeeBankAccount.java
@@ -2,9 +2,11 @@ package it.unibo.pps.model;
 
 public class FeeBankAccount implements BankAccount {
     private final BankAccount bankAccount;
+    private final double withdrawalFee;
 
     public FeeBankAccount(BankAccount bankAccount, double withdrawalFee) {
         this.bankAccount = bankAccount;
+        this.withdrawalFee = withdrawalFee;
     }
 
     @Override
@@ -19,6 +21,6 @@ public class FeeBankAccount implements BankAccount {
 
     @Override
     public void withdraw(int userID, double amount) {
-        this.bankAccount.withdraw(userID, amount);
+        this.bankAccount.withdraw(userID, amount + this.withdrawalFee);
     }
 }
--- basic-refactoring-exercise/src/test/java/it/unibo/pps/model/FeeBankAccountTest.java
+++ basic-refactoring-exercise/src/test/java/it/unibo/pps/model/FeeBankAccountTest.java
@@ -1,10 +1,22 @@
 package it.unibo.pps.model;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FeeBankAccountTest extends AbstractBankAccountTest {
     private static final double WITHDRAWAL_FEE = 1;
 
     @Override
     protected BankAccount createBankAccountUnderTest() {
         return new FeeBankAccount(new SimpleBankAccount(this.accountHolder, INITIAL_BALANCE), WITHDRAWAL_FEE);
+    }
+
+    @Test
+    @Override
+    void testWithdraw() {
+        this.bankAccount.deposit(this.accountHolder.id(), DEPOSIT_AMOUNT);
+        this.bankAccount.withdraw(this.accountHolder.id(), WITHDRAWAL_AMOUNT);
+        assertEquals(DEPOSIT_AMOUNT - WITHDRAWAL_AMOUNT - WITHDRAWAL_FEE, this.bankAccount.getBalance());
     }
 }
