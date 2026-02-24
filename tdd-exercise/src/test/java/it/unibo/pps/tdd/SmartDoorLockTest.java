b020d9910cd637967f3df24ce96d12da0398bebb
Andrea Cecchini <andrea.cecchini13@studio.unibo.it>
2026-02-24 17:00:37 +0100

    refactor: common tests have been moved to an abstract class

--- tdd-exercise/src/test/java/it/unibo/pps/tdd/SmartDoorLockTest.java
+++ tdd-exercise/src/test/java/it/unibo/pps/tdd/SmartDoorLockTest.java
@@ -2,27 +2,17 @@ package it.unibo.pps.tdd;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class SmartDoorLockTest {
-    private static final int MAX_PIN_LENGTH = 4;
+public abstract class SmartDoorLockTest {
     private static final int PIN = 1234;
     private static final int WRONG_PIN = 0;
-    private static final int MAX_ATTEMPTS = 10;
-    private SmartDoorLock smartDoorLockUnderTest;
+    protected SmartDoorLock smartDoorLockUnderTest;
 
     @BeforeEach
     void beforeEach() {
-        final PinValidator lengthValidator = pin -> pin >= Math.powExact(10, MAX_PIN_LENGTH - 1) && pin <= Math.powExact(10, MAX_PIN_LENGTH) - 1;
-        this.smartDoorLockUnderTest = new SmartDoorWithMaxAttempts(MAX_ATTEMPTS, lengthValidator);
-    }
-
-    @Test
-    void testMaxNumberOfAttemptsShouldCorrectlySet() {
-        assertEquals(MAX_ATTEMPTS, this.smartDoorLockUnderTest.getMaxAttempts());
+        this.smartDoorLockUnderTest = createSmartDoorLock();
     }
 
     @Test
@@ -34,13 +24,6 @@ public class SmartDoorLockTest {
     void testShouldBePossibleToSetPinAfterInitialization() {
         assertDoesNotThrow(() -> this.smartDoorLockUnderTest.setPin(PIN));
         assertIsOpen();
-    }
-
-    @ParameterizedTest
-    @ValueSource(ints = {0, 1, 12, 123, 1234567, -123, -1234})
-    void testPinShouldThrowExceptionForInvalidLength(int wrongLengthPin) {
-        assertThrows(IllegalArgumentException.class,
-                () -> this.smartDoorLockUnderTest.setPin(wrongLengthPin));
     }
 
     @Test
@@ -62,12 +45,6 @@ public class SmartDoorLockTest {
     }
 
     @Test
-    void testAttemptsShouldBeZeroAfterLock() {
-        lockSmartDoor();
-        assertEquals(0, this.smartDoorLockUnderTest.getFailedAttempts());
-    }
-
-    @Test
     void testShouldBePossibleToUnlockWithTheRightPin() {
         lockSmartDoor();
         this.smartDoorLockUnderTest.unlock(PIN);
@@ -82,76 +59,21 @@ public class SmartDoorLockTest {
     }
 
     @Test
-    void testFailedAttemptsShouldIncreaseWhenPinIsWrong() {
-        lockSmartDoor();
-        for (int i = 1; i <= MAX_ATTEMPTS; i++) {
-            this.smartDoorLockUnderTest.unlock(WRONG_PIN);
-            assertEquals(i, this.smartDoorLockUnderTest.getFailedAttempts());
-        }
-    }
-
-    @Test
-    void testShouldBeBlockedAfterReachingMaximumAttempts() {
-        blockSmartDoor();
-        assertTrue(this.smartDoorLockUnderTest.isBlocked());
-    }
-
-    @Test
-    void testShouldNotBePossibleToAttemptIfBlocked() {
-        blockSmartDoor();
-        this.smartDoorLockUnderTest.unlock(PIN);
-        assertTrue(this.smartDoorLockUnderTest.isBlocked());
-    }
-
-    @Test
-    void testShouldBeOpenAfterResetWhenBlocked() {
-        blockSmartDoor();
-        this.smartDoorLockUnderTest.reset();
-        assertIsOpen();
-    }
-
-    @Test
     void testPinShouldNotBeSetAfterReset() {
         this.smartDoorLockUnderTest.setPin(PIN);
         this.smartDoorLockUnderTest.reset();
         assertThrows(IllegalStateException.class, () -> this.smartDoorLockUnderTest.lock());
     }
 
-    @Test
-    void testNumberOfFailedAttemptsShouldBeZeroAfterReset() {
-        blockSmartDoor();
-        this.smartDoorLockUnderTest.reset();
-        assertEquals(0, this.smartDoorLockUnderTest.getFailedAttempts());
-    }
+    protected abstract SmartDoorLock createSmartDoorLock();
 
-    @Test
-    void testNumberOfFailedAttemptsShouldBeZeroAfterUnlocking() {
-        failUnlockingUntilLastAttempt();
-        this.smartDoorLockUnderTest.unlock(PIN);
-        assertEquals(0, this.smartDoorLockUnderTest.getFailedAttempts());
-    }
-
-    private void assertIsOpen() {
+    protected void assertIsOpen() {
         assertFalse(this.smartDoorLockUnderTest.isLocked());
         assertFalse(this.smartDoorLockUnderTest.isBlocked());
     }
 
-    private void lockSmartDoor() {
+    protected void lockSmartDoor() {
         this.smartDoorLockUnderTest.setPin(PIN);
         this.smartDoorLockUnderTest.lock();
-    }
-
-    private void blockSmartDoor() {
-        lockSmartDoor();
-        for (int i = 1; i <= MAX_ATTEMPTS; i++) {
-            this.smartDoorLockUnderTest.unlock(WRONG_PIN);
-        }
-    }
-
-    private void failUnlockingUntilLastAttempt() {
-        lockSmartDoor();
-        for (int i = 1; i <= MAX_ATTEMPTS - 1; i++) {
-            this.smartDoorLockUnderTest.unlock(WRONG_PIN);
-        }
     }
 }
---
+++ tdd-exercise/src/test/java/it/unibo/pps/tdd/SmartDoorLockWithMaxAttemptsTest.java
@@ -0,0 +1,96 @@
+package it.unibo.pps.tdd;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SmartDoorLockWithMaxAttemptsTest extends SmartDoorLockTest {
+    private static final int MAX_PIN_LENGTH = 4;
+    private static final int PIN = 1234;
+    private static final int WRONG_PIN = 0;
+    private static final int MAX_ATTEMPTS = 10;
+
+    @Override
+    protected SmartDoorLock createSmartDoorLock() {
+        final PinValidator lengthValidator = pin -> pin >= Math.powExact(10, MAX_PIN_LENGTH - 1) && pin <= Math.powExact(10, MAX_PIN_LENGTH) - 1;
+        return new SmartDoorWithMaxAttempts(MAX_ATTEMPTS, lengthValidator);
+    }
+
+    @Test
+    void testMaxNumberOfAttemptsShouldCorrectlySet() {
+        assertEquals(MAX_ATTEMPTS, this.smartDoorLockUnderTest.getMaxAttempts());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 12, 123, 1234567, -123, -1234})
+    void testPinShouldThrowExceptionForInvalidLength(int wrongLengthPin) {
+        assertThrows(IllegalArgumentException.class,
+                () -> this.smartDoorLockUnderTest.setPin(wrongLengthPin));
+    }
+
+    @Test
+    void testAttemptsShouldBeZeroAfterLock() {
+        lockSmartDoor();
+        assertEquals(0, this.smartDoorLockUnderTest.getFailedAttempts());
+    }
+
+    @Test
+    void testFailedAttemptsShouldIncreaseWhenPinIsWrong() {
+        lockSmartDoor();
+        for (int i = 1; i <= MAX_ATTEMPTS; i++) {
+            this.smartDoorLockUnderTest.unlock(WRONG_PIN);
+            assertEquals(i, this.smartDoorLockUnderTest.getFailedAttempts());
+        }
+    }
+
+    @Test
+    void testShouldBeBlockedAfterReachingMaximumAttempts() {
+        blockSmartDoor();
+        assertTrue(this.smartDoorLockUnderTest.isBlocked());
+    }
+
+    @Test
+    void testShouldNotBePossibleToAttemptIfBlocked() {
+        blockSmartDoor();
+        this.smartDoorLockUnderTest.unlock(PIN);
+        assertTrue(this.smartDoorLockUnderTest.isBlocked());
+    }
+
+    @Test
+    void testShouldBeOpenAfterResetWhenBlocked() {
+        blockSmartDoor();
+        this.smartDoorLockUnderTest.reset();
+        assertIsOpen();
+    }
+
+    @Test
+    void testNumberOfFailedAttemptsShouldBeZeroAfterReset() {
+        blockSmartDoor();
+        this.smartDoorLockUnderTest.reset();
+        assertEquals(0, this.smartDoorLockUnderTest.getFailedAttempts());
+    }
+
+    @Test
+    void testNumberOfFailedAttemptsShouldBeZeroAfterUnlocking() {
+        failUnlockingUntilLastAttempt();
+        this.smartDoorLockUnderTest.unlock(PIN);
+        assertEquals(0, this.smartDoorLockUnderTest.getFailedAttempts());
+    }
+
+    private void blockSmartDoor() {
+        lockSmartDoor();
+        for (int i = 1; i <= MAX_ATTEMPTS; i++) {
+            this.smartDoorLockUnderTest.unlock(WRONG_PIN);
+        }
+    }
+
+    private void failUnlockingUntilLastAttempt() {
+        lockSmartDoor();
+        for (int i = 1; i <= MAX_ATTEMPTS - 1; i++) {
+            this.smartDoorLockUnderTest.unlock(WRONG_PIN);
+        }
+    }
+}
