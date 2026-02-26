11e670b86bfaf970a4c3714793c16911567ce323
Andrea Cecchini <andrea.cecchini13@studio.unibo.it>
2026-02-26 12:05:23 +0100

    doc: added javadoc to describe implementation

--- tdd-exercise/src/main/java/it/unibo/pps/tdd/CircularList.java
+++ tdd-exercise/src/main/java/it/unibo/pps/tdd/CircularList.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * A {@link CircularQueue} implementation based on {@link List}.
+ */
 public class CircularList implements CircularQueue {
     final List<Integer> buffer;
     int front = 0;
